88c91452666664075da950b5953fdb38c34dc2d3
dylan <d1stahl@gmail.com>
2017-04-30 18:38:00 -0400

    Initial commit

---
+++ app/src/main/java/com/dystahl/classlib/MainMenu.java
@@ -0,0 +1,45 @@
+package com.dystahl.classlib;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main_menu);
+
+        SQLiteDatabase libraryDB = openOrCreateDatabase("Library", MODE_PRIVATE, null);
+
+        libraryDB.execSQL("CREATE TABLE IF NOT EXISTS BOOK(ISBN INT NOT NULL, TITLE VARCHAR NOT NULL," +
+                " AUTHOR VARCHAR NOT NULL, BINDING VARCHAR, LENGTH INT, GENRE VARCHAR, PRIMARY KEY(ISBN) )");
+
+        libraryDB.execSQL("CREATE TABLE IF NOT EXISTS STUDENT(ID INT NOT NULL, FNAME VARCHAR NOT NULL," +
+                " LNAME VARCHAR NOT NULL, CONTACT VARCHAR NOT NULL, PERIOD INTEGER, PRIMARY KEY(ID))");
+
+        libraryDB.execSQL("CREATE TABLE IF NOT EXISTS CHECKOUT( ISBN INT NOT NULL, ID INT NOT NULL, PRIMARY KEY (ISBN, ID)," +
+                "FOREIGN KEY(ISBN) REFERENCES BOOK, FOREIGN KEY(ID) REFERENCES STUDENT)");
+
+        Cursor resultSet = libraryDB.rawQuery("SELECT * FROM BOOK", null);
+
+        resultSet.moveToFirst();
+
+        Toast justBurnt = Toast.makeText(this, resultSet.getString(1), Toast.LENGTH_SHORT);
+        justBurnt.show();
+
+
+    }
+}
